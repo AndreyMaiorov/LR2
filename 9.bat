de6ade7ebe5a4932a81347ce708b32e3e24c580c
AndreyMaiorov <134500970+AndreyMaiorov@users.noreply.github.com>
2023-06-02 14:41:06 +0300

    Add files via upload

---
+++ 9.bat
@@ -0,0 +1,23 @@
+echo off
+md mayorov
+cd mayorov
+md andrey
+md maximovich
+pause
+echo off
+echo > 02052002.txt
+cd maximovich
+echo > msi.txt
+cd ..
+cd ..
+pause
+echo off
+del mayorov /S /Q /F
+pause
+echo off
+cd mayorov 
+rd andrey
+rd maximovich
+cd .. 
+rd mayorov 
+pause
